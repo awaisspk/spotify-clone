bd0e58f41115ab823c6562753fd3f8338d1e9efc
awaisspk <iamawaisiqball@gmail.com>
2021-12-31 10:49:22 +0500

    user profile

---
+++ components/gradientLayout.tsx
@@ -0,0 +1,40 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+
+export const GradientLayout = ({
+  color,
+  children,
+  image,
+  subtitle,
+  title,
+  description,
+  roundImage,
+}) => {
+  return (
+    <Box
+      height="100%"
+      overflowY="auto"
+      bgGradient={`linear(${color}.500 0% , ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.9) 75%)`}
+    >
+      <Flex bg={`${color}.600`} alignItems="center" color="white">
+        <Box padding="20px">
+          <Image
+            boxSize="160px"
+            boxShadow="2xl"
+            src={image}
+            borderRadius={roundImage ? "100%" : "3px"}
+          />
+        </Box>
+        <Box padding="20px" lineHeight="40px">
+          <Text fontSize="x-small" fontWeight="boo" casing="uppercase">
+            {subtitle}
+          </Text>
+          <Text fontSize="6xl">{title}</Text>
+          <Text fontSize="x-small" fontWeight="100">
+            {description}
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
